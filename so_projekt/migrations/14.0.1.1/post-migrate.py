6df915f81e25826e8230f0071303a44163abc551
Intissar Boussetta <iboussetta@hadooc.com>
2021-03-31 17:50:20 +0100

    Generate customer number automatically

--- so_projekt/migrations/14.0.1.1/post-migrate.py
+++ so_projekt/migrations/14.0.1.1/post-migrate.py
@@ -4,19 +4,20 @@ from odoo.api import Environment
 def migrate(cr, version):
     """Update Invoices."""
     env = Environment(cr, 1, context={})
-    invoices = env["account.move"].sudo().search([('move_type', '=', 'out_invoice'), ('name', '!=', '/')], order="invoice_date, name")
+    invoices = env["account.move"].sudo().search([('move_type', '=', 'out_invoice'), ('state', '=', 'posted'), ('name', '!=', '/')], order="invoice_date, name")
+    invoices_not_migrated = env["account.move"].sudo().search([('move_type', '=', 'out_invoice'), ('state', '=', 'posted'), ('name', 'ilike', '/')])
     print("Start change old invoice number")
-    first_invoice = invoices[0]
-    code_first_invoice = invoices[0].name.split('/', 4)
-    invoices[0].name = "RG_10_%s" % str(code_first_invoice[3])
-    invoices[0].payment_reference = invoices[0].name
-    for invoice in invoices:
-        if invoice != first_invoice:
-            code = invoice.name.split('/', 4)
-            new_code = int(code[3]) + 1
-            invoice.name = "RG_10_%04d" % new_code
-            invoice.payment_reference = invoice.name
-
+    if invoices_not_migrated:
+        first_invoice = invoices[0]
+        code_first_invoice = invoices[0].name.split('/', 4)
+        invoices[0].name = "RG_10_%s" % str(code_first_invoice[3])
+        invoices[0].payment_reference = invoices[0].name
+        for invoice in invoices:
+            if invoice != first_invoice:
+                code = invoice.name.split('/', 4)
+                new_code = int(code[3]) + 1
+                invoice.name = "RG_10_%04d" % new_code
+                invoice.payment_reference = invoice.name
     print("Finish change old invoice number")
     print("Start add automatically ref to partner")
     partners = env["res.partner"].sudo().search([('ref', '=', False)], order="create_date")
